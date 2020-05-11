58481825cb708e652fdc1f20cb7224d7fa28e259
Ram Akuka <ramakuka@gmail.com>
2020-05-11 12:03:11 -0700

    Refactor the print_result so it can easily support error

--- src/print_result.py
+++ src/print_result.py
@@ -1,3 +1,4 @@
+from functools import wraps
 SUCCESS_MESSAGE = "No Violations Detected"
 DEFAULT_ROW_SIZE = 100
 
@@ -5,30 +6,40 @@ DEFAULT_ROW_SIZE = 100
 def print_results(violations):
     result = []
     if len(violations.summary.get()) == 0:
-        result.extend(_wrap_text(DEFAULT_ROW_SIZE, _text(SUCCESS_MESSAGE, DEFAULT_ROW_SIZE)))
+        t = TextWrapper(DEFAULT_ROW_SIZE)
+        result.extend(t.headline(SUCCESS_MESSAGE))
     else:
         " Set the size of the header and footer by the number of charters in the violation table "
         v_list = violations.violations_list.pritify()
         row_size = len(v_list.split("\n")[0])
+        t = TextWrapper(row_size)
         " Set the summary section "
-        result.extend(_wrap_text(row_size, _text("Result Summary:", row_size)))
-        result.extend(violations.summary.pritify().split('\n'))
-        result.extend([_header_footer(row_size), "\n", "\n"])
+        result.extend(block(t, "Result Summary:", violations.summary.pritify().split('\n')))
         " Set the details section "
-        result.extend(_wrap_text(row_size, _text("Violations Details:", row_size)))
-        result.extend(v_list.split("\n"))
-        result.extend([_header_footer(row_size), "\n", "\n"])
+        result.extend(block(t, "Violations Details:", v_list.split("\n")))
+        " Set the Error section"
     return '\n'.join(result)
 
 
-def _wrap_text(row_size, text):
-    return[_header_footer(row_size), text, _header_footer(row_size)]
+def block(t_hander, headline_text, data):
+    r = []
+    r.extend(t_hander.headline(headline_text))
+    r.extend(data)
+    r.extend([t_hander.line, "\n", "\n"])
+    return r
 
 
-def _header_footer(row_size):
-    return "#" * row_size
+class TextWrapper():
+    def __init__(self, row_size):
+        self.row_size = row_size
 
+    @property
+    def line(self):
+        return "#" * self.row_size
 
-def _text(text, row_size):
-    spaces = "".join([" " for x in range(int((row_size - (len(text)+2)) / 2))])
-    return ("#{}{}{}#".format(spaces, text, spaces))
+    def wrapped_text(self, text):
+        spaces = "".join([" " for x in range(int((self.row_size - (len(text)+2)) / 2))])
+        return ("#{}{}{}#".format(spaces, text, spaces))
+
+    def headline(self, headline_text):
+        return [self.line, self.wrapped_text(headline_text), self.line]
--- src/validate.py
+++ src/validate.py
@@ -6,15 +6,18 @@ from print_result import print_results
 import json
 
 
+def validate():
+    violations = ProcessViolations().run()
+    if Config.output_file:
+        if len(violations.summary.get()):
+            with open(Config.output_file, 'w') as fd:
+                json.dump(violations.violations_list.get(), fd, indent=4, separators=(',', ': '), sort_keys=True)
+        else:
+            json.dump({}, fd, indent=4, separators=(',', ': '), sort_keys=True)
+    else:
+        print(print_results(violations))
+
+
 if __name__ == "__main__":
     Config.validate_config()
-    violations = ProcessViolations().run()
-    if len(violations.summary.get()) > 0:
-        if Config.output_file:
-            if len(violations.summary.get()):
-                with open(Config.output_file, 'w') as fd:
-                    json.dump(violations.violations_list.get(), fd, indent=4, separators=(',', ': '), sort_keys=True)
-        else:
-            print(print_results(violations))
-    else:
-        print("No violations has been detected!")
+    validate()
